41dc32b4df09b02511cf674343ad05be949bd289
patrick andrews <patrickxandrews@gmail.com>
2023-03-27 13:22:59 -0600

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,125 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <Product
+      v-for="product of products"
+      :key="product.title"
+      :img="product.img"
+      :title="product.title"
+      :caption="product.caption"
+      :color="product.color"
+    />
+    <!-- <Product
+      :key="products[currentProduct].title"
+      :img="products[currentProduct].img"
+      :title="products[currentProduct].title"
+      :caption="products[currentProduct].caption"
+      :color="products[currentProduct].color"
+    /> -->
+    <!-- <button @click="next()">next</button>
+    <button @click="prev()">prev</button> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from "./components/Product.vue";
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      // light then dark, mellona, pomona, khloris, tonic, water, accent gold color last
+      colors: [
+        "#A191B2",
+        "#003B4A",
+        "#BDCF9B",
+        "#1A5632",
+        "#EFB8A9",
+        "#702E3E",
+        "#B2E1D8",
+        "#003B4A",
+        "#FFFFFF",
+        "#415364",
+        "#AE841F",
+      ],
+      currentProduct: 0,
+      products: [
+        {
+          img: require("./assets/images/Khloris Can Mock.png"),
+          title: "Khloris",
+          caption: "Floral Herbal Tea with Honey and Mosaic Hops",
+          color: "#702E3E",
+          bg: "#EFB8A9",
+        },
+        {
+          img: require("./assets/images/Mellona Can Mock.png"),
+          title: "Mellona",
+          caption: "Earl Grey Tea with Honey",
+          color: "#003B4A",
+          bg: "#A191B2",
+        },
+        {
+          img: require("./assets/images/Pomona Can Mock.png"),
+          title: "Pomona",
+          caption: "Fruity Green Tea with Honey and Mosaic Hops",
+          color: "#1A5632",
+          bg: "#BDCF9B",
+        },
+        {
+          img: require("./assets/images/Sparkling Can Mock.png"),
+          title: "Sparkling Water",
+          caption: "Simple, Light and Bubbly Sparkling Water",
+          color: "#415364",
+          bg: "#FFFFFF",
+        },
+        {
+          img: require("./assets/images/Tonic Can Mock.png"),
+          title: "Tonic Water",
+          caption: "Deliciously Crisp Citrus Based Tonic",
+          color: "#003B4A",
+          bg: "#B2E1D8",
+        },
+      ],
+    };
+  },
+  methods: {
+    next() {
+      this.currentProduct++;
+      if (this.currentProduct > this.products.length - 1)
+        this.currentProduct = 0;
+    },
+    prev() {
+      this.currentProduct--;
+      if (this.currentProduct < 0)
+        this.currentProduct = this.products.length - 1;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    Product,
+    Header,
+  },
+};
 </script>
 
 <style>
+@font-face {
+  font-family: "BNMainz";
+  src: url("./assets/fonts/BNMainz.woff2") format("woff2"),
+    url("./assets/fonts/BNMainz.woff") format("woff");
+}
+@font-face {
+  font-family: "MyWebFont";
+  src: url("./assets/fonts/Nightingale.woff2") format("woff2"),
+    url("./assets/fonts/Nightingale.woff") format("woff");
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  align-content: center;
 }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="product">
+    <div class="product-wrapper">
+      <div class="title" :style="{ color: color }">{{ title }}</div>
+      <img class="image" v-bind:src="img" />
+      <div class="caption" :color="color">{{ caption }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "Product",
+  props: {
+    img: String,
+    title: String,
+    caption: String,
+    color: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.product {
+  width: 40%;
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+}
+.title {
+  font-size: 3rem;
+  font-family: "BNMainz";
+}
+.image {
+  width: 100%;
+}
+.caption {
+  font-size: 1.2rem;
+  font-family: "Nightingale";
+}
+</style>
